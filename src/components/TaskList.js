1582505f5193b1c708d6efaabfedd9e411aee07e
Malgorzata Nowak <nowak.em@interia.eu>
2022-10-23 14:56:11 +0200

    list sort added

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -6,6 +6,30 @@ const TaskList = (props) => {
     const done = props.tasks.filter(task => !task.active);
     //console.log(active, done);
 
+    if (done.length >= 2) {
+        done.sort((a, b) => {
+            if (a.finishDate < b.finishDate) {
+                return 1
+            }
+            if (a.finishDate > b.finishDate) {
+                return -1
+            }
+            return 0
+        })
+    };
+
+    if (active.length >= 2) {
+        active.sort((a, b) => {
+
+            a = a.text.toLowerCase();
+            b = b.text.toLowerCase();
+            if (a < b) return -1;
+            if (a > b) return 1;
+            return 0;
+        })
+    }
+
+
     const activeTasks = active.map(task => <Task key={task.id} task={task} delete={props.delete} change={props.change} />);
 
     const doneTasks = done.map(task => <Task key={task.id} task={task} delete={props.delete} change={props.change} />)
